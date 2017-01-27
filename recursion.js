25ddd09038a9729fe38fb5cb1cb9bc070dc0ddb4
nicoHz <nicoHz@posteo.de>
2017-01-27 19:48:21 +0100

    A recursion and an a nested function example

---
+++ recursion.js
@@ -0,0 +1,35 @@
+/*function isEven(number) {
+	if(number == 1) {
+		return false;
+	}
+	else if (number === 0) { 
+		return true;
+	} else if (number < 0) {
+		return isEven(-number); 
+	} else {
+		return isEven(number - 2);
+	}
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(0));
+console.log(isEven(-1));
+*/
+
+/*###################################### */
+// exampe for nested function 
+
+function multiplyBy10(number) {
+	console.log(number * 10);
+}
+
+function multiplesOf10(limit) {
+	// Put your for loop here
+	for(var i = 1; i <= limit; i++) {
+		var result = multiplyBy10(i);
+	} return result;
+}
+
+// Call the function multiplesOf10() on any integer
+multiplesOf10(3);
