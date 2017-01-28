390c6f8ea9bac97784eda56885978ac0d96459f2
nicoHz <nicoHz@posteo.de>
2017-01-28 20:20:07 +0100

    Add function which is counting choosen characters of a string.

--- recursion.js
+++ recursion.js
@@ -1,4 +1,4 @@
-/*function isEven(number) {
+function isEven(number) {
 	if(number == 1) {
 		return false;
 	}
@@ -15,7 +15,7 @@ console.log(isEven(50));
 console.log(isEven(75));
 console.log(isEven(0));
 console.log(isEven(-1));
-*/
+
 
 /*###################################### */
 // exampe for nested function 
@@ -33,3 +33,24 @@ function multiplesOf10(limit) {
 
 // Call the function multiplesOf10() on any integer
 multiplesOf10(3);
+
+////////////////////////////////////////////////////////////
+
+
+function countLetter(string, letter) {
+	var counter = 0;
+	for (var i = 0; i < string.length; i++) {
+		if (string.charAt(i) == letter)
+			counter += 1;
+		}
+		return counter;
+	}
+	
+function countBs(string) {
+	return countLetter(string, "B");
+}
+
+console.log(countBs("some B & B in a B string"));
+console.log(countLetter("ajksjdhjsdjj", "j"));
+
+
